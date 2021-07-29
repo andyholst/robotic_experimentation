596b5cf5235d63fbe4cfa94ad7fe3458b60bc73a
Andy Holst <andyholst@vivaldi.net>
2021-07-29 13:09:37 +0200

    Added launch file to support several nodes etc
    Added a launch file to start all related nodes and topics etc.
    
    Updated the ROS2 Dockerfile and replaced run with launch command
    instead.
    
    Updated setup.py file to include all related launch files when
    installing the 'robot_prototype' package on the ROS2 platform.

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ RUN mkdir -p /ros2_home/src
 WORKDIR /ros2_home/src
 
 CMD ["bash", "-c", "colcon build --packages-select robot_prototype && \
-. install/local_setup.bash && ros2 run robot_prototype robot_node"]
+. install/local_setup.bash && \
+ros2 launch robot_prototype robot_prototype_launch.py && \
+tail -f /dev/null"]
--- robot_prototype/setup.py
+++ robot_prototype/setup.py
@@ -13,6 +13,8 @@
 #  limitations under the License.
 
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'robot_prototype'
 
@@ -24,6 +26,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
