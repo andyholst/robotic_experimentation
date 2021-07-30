f0370d68b7dce9ce7179fee401da21038ebc2045
Andy Holst <andyholst@vivaldi.net>
2021-07-30 12:08:58 +0200

    Added Fusion360 in Docker setup
    Updated README.md file how to execute AutoDesk Fusion360 modeling
    tool in Docker.
    
    Added Dockerfile setup for Fusion360 to be executed with WINE
    Is Not an Emulator.

---
+++ fusion360_in_docker/install_fusion360.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" WINEDLLOVERRIDES="mscoree,mshtml=" wine wineboot
+
+wget -nc "https://github.com/madewokherd/wine-mono/releases/download/wine-mono-6.3.0/wine-mono-6.3.0-x86.msi"
+wget -nc "http://dl.winehq.org/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86_64.msi"
+
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" ${WINE} msiexec /i wine-mono-6.3.0-x86.msi
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" ${WINE} msiexec /i wine-gecko-2.47.2-x86_64.msi
+
+rm wine-mono-6.3.0-x86.msi
+rm wine-gecko-2.47.2-x86_64.msi
+
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q atmlib
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q gdiplus
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q corefonts
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q fontsmooth=rgb
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q winhttp
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q vcrun2017
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q dxvk
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks vulcan=builtin
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks vcruntime140_1=native
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks d3d9=builtin
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks d3d11=native
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q win10
+
+touch log.txt
+./watch_fusion360_install_proccess.sh &
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" ${WINE} "${HOME}/setup/streamer.exe" -p deploy -g -f log.txt --quiet
