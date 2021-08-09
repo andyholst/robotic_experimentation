a427e67d448fc5fda9226b74cee9e8c18cbefa67
Andy Holst <andyholst@vivaldi.net>
2021-08-09 16:49:32 +0200

    Improved watch fusion 360 script
    Improved watch_fusion360_install_proccess.sh script so the generate
    the FUSION_360_EXE path is done by looking at the fusion360 WINE
    directory instead of the ubuntu home directory to minimize future
    conflicts.

--- fusion360_in_docker/watch_fusion360_install_proccess.sh
+++ fusion360_in_docker/watch_fusion360_install_proccess.sh
@@ -20,7 +20,7 @@ done
 rm -fr "${HOME}/setup"
 rm log.txt
 
-FUSION_360_EXE="$(find ${HOME} -name "FusionLauncher.exe.ini" | head -1 | xargs -I '{}' echo {} | head -c-5)"
+FUSION_360_EXE="$(find "${WINE_PATH}" -name "FusionLauncher.exe.ini" | head -1 | xargs -I '{}' echo {} | head -c-5)"
 FUSION_360_EXE="$(echo ${FUSION_360_EXE} | sed -e 's/Program Files/Program\\ Files/g')"
 echo "export FUSION_360_EXE=${FUSION_360_EXE}" >> "${HOME_PATH}/.bashrc"
 source "${HOME_PATH}/.bashrc"
