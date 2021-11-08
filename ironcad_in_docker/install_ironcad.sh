1a6154efa59847ee844046c2d06bca431eb2ccf5
Andy Holst <andyholst@vivaldi.net>
2021-11-08 02:12:00 +0100

    Add more IronCAD dependencies
    Added more IronCAD dependencies to get importing of an existing 3D
    shape loaded and rendered on 3D scene.

--- ironcad_in_docker/install_ironcad.sh
+++ ironcad_in_docker/install_ironcad.sh
@@ -13,7 +13,28 @@ rm wine-gecko-2.47.2-x86_64.msi
 
 wineserver -k
 
+WINEPREFIX=~/.wine32 WINEARCH=win32 winetricks -q ole32
+
+
+# Create Win32 bottle
+# modify arch=win32 to arch=win64
+# /home/winer/.wine32/system.reg:#arch=win32
+#/home/winer/.wine32/userdef.reg:#arch=win32
+#/home/winer/.wine32/user.reg:#arch=win32
+# Install DLLS ole32
+# Download IronCad
+WINEPREFIX=~/.wine32 WINEARCH=win32 wineboot
+
+
+
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q msxml6
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q gdiplus
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q winhttp
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q vcrun2017
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q dotnet48
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q ole32
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q oleaut32
+WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q mdac28
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q d3dcompiler_47
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q dxvk
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q win10
@@ -21,6 +42,9 @@ WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" winetricks -q win10
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" ${WINE} IronCADDCS2019_x64.exe
 
 # You might have to use super permissions or add permissions to the Vulkan library for being able to transform
-# DirectX 9/11 to Vulkan rendering
+# DirectX 9/11 to Vulkan rendering like 'chown -R root "${WINE_PATH}"', you are executing in container context so it's
+# OK, but not on a host machine
+# WINEDEBUG=+loaddll get dll dependencies to resolve missing dependencies
+
 WINEPREFIX="${WINE_PATH}" WINEARCH="${ARCHITECTURE}" ${WINE} \
 "${WINE_PATH}"/drive_c/Program\ Files/IronCAD/2019/bin/IRONCAD.exe
